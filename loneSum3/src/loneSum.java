ed484afd7141d9b48eb491b75c171f5c557e8626
Maryna Kuzminchuk <Maryna.Kuzminchuk@Student.HTW-Berlin.de>
2021-04-25 19:53:35 +0200

    loneSum solved

--- loneSum3/src/loneSum.java
+++ loneSum3/src/loneSum.java
@@ -1,13 +1,24 @@
 public class loneSum {
 
 	public static void main(String[] args) {
+		System.out.println(loneSum(1,5,5));
 	}
 	
 	// Given 3 int values, a b c, return their sum. However,
 	// if one of the values is the same as another of the values, 
 	// it does not count towards the sum.
 	
-	public int loneSum(int a, int b, int c) {
-		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	public static int loneSum(int a, int b, int c) {
+		if(a==b && b==c) {
+			return 0;
+		} else if(a==b) {
+			return c;
+		} else if(a==c) {
+			return b;
+		} else if(b==c) {
+			return a;
+		} else {
+			return a+b+c;
+		}
 	}
 }
