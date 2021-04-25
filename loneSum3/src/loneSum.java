1b749a5fca211ee9704c6464590b672ae92de894
MAHAD_YOUSAFZAI <mahad.yousafzai@student.htw-berlin.de>
2021-04-25 19:25:15 +0200

    Adding Java puzzle

---
+++ loneSum3/src/loneSum.java
@@ -0,0 +1,13 @@
+public class loneSum {
+
+	public static void main(String[] args) {
+	}
+	
+	// Given 3 int values, a b c, return their sum. However,
+	// if one of the values is the same as another of the values, 
+	// it does not count towards the sum.
+	
+	public int loneSum(int a, int b, int c) {
+		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	}
+}
