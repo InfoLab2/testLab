2f592a742c38607bf7843cb0618b1cf6ee1ca9a9
Maryna Kuzminchuk <Maryna.Kuzminchuk@Student.HTW-Berlin.de>
2021-04-25 19:27:50 +0200

    Added puzzle

---
+++ puzzle1/src/puzzle1/Main.java
@@ -0,0 +1,17 @@
+package puzzle1;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		System.out.println(" ");
+
+	}
+	// Given 2 int values greater than 0, and the method returns
+	// whichever value is nearest to 21 without going over. 
+	// It returns 0 if they both go over.
+	public int blackjack(int a, int b) {
+		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	}
+
+}
